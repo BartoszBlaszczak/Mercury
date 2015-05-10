23795b10061f08b311e24d7fb7448ef0c3918ab1
Bartosz Błaszczak <bartosz.piotr.blaszczak@gmail.com>
2015-05-10 22:51:28 +0200

    .

--- mercury-server/test/pl/uz/mercury/service/MercuryTest.java
+++ mercury-server/test/pl/uz/mercury/service/MercuryTest.java
@@ -1,15 +1,37 @@
 package pl.uz.mercury.service;
 
+import static org.junit.Assert.assertEquals;
+
+import javax.ejb.EJB;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
-import pl.uz.mercury.exception.EntityDtoAssignException;
+import pl.uz.mercury.util.PropertiesReader;
+import pl.uz.mercury.util.PropertiesReader.PropertyName;
 
+@RunWith(Arquillian.class)
 public class MercuryTest
 {
+	@EJB
+	PropertiesReader	propertiesReader;
+
+	@Deployment
+	public static JavaArchive createArchiveAndDeploy ()
+	{
+		return ShrinkWrap.create(JavaArchive.class).addClasses(PropertiesReader.class)
+				.addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+	}
 
 	@Test
-	public void createEntityInstanceTest() throws InstantiationException, IllegalAccessException, EntityDtoAssignException
+	public void Test ()
 	{
-		
+		String property = propertiesReader.getProperty(PropertyName.PERSISTER_NAME);
+		assertEquals("mercuryPersister", property);
 	}
 }
